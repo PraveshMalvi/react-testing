75ba79a785bc1fb1f39822a833f1de83c8594a4c
praveshScube <pravesh@scube.me>
2025-04-11 09:52:52 +0530

    tested with mock function

---
+++ src/components/counter-two/CounterTwo.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react";
+import CounterTwo from "./CounterTwo";
+import userEvent from "@testing-library/user-event";
+
+describe("CounterTwo", () => {
+  test("renders text correctly", () => {
+    render(<CounterTwo count={0} />);
+    const textElement = screen.getByText("Counter Two");
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test("handelers are called", async () => {
+    userEvent.setup();
+    const incrementHandeler = jest.fn();
+    const decrementHandeler = jest.fn();
+    render(
+      <CounterTwo
+        count={0}
+        handleIncrement={incrementHandeler}
+        handleDecrement={decrementHandeler}
+      />
+    );
+    const incrementBtn = screen.getByRole("button", {
+        name: "Increment"
+    })
+    const decrementBtn = screen.getByRole("button", {
+        name: "Decrement"
+    })
+    await userEvent.click(incrementBtn)
+    await userEvent.click(decrementBtn)
+    expect(incrementHandeler).toHaveBeenCalledTimes(1)
+    expect(decrementHandeler).toHaveBeenCalledTimes(1)
+  });
+});
---
+++ src/components/counter-two/CounterTwo.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { CounterTwoProps } from "./CounterTwo.types";
+
+const CounterTwo = (props: CounterTwoProps) => {
+  return (
+    <div>
+      <h1>Counter Two</h1>
+      <p>{props.count}</p>
+     
+        <button onClick={props.handleIncrement}>Increment</button>
+      
+        <button onClick={props.handleDecrement}>Decrement</button>
+      
+    </div>
+  );
+};
+
+export default CounterTwo;
